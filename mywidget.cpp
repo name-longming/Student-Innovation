abebc5acd61922af8687ebf89dc72ffe6437ba06
name-longming <1756284275@qq.com>
2021-01-17 23:21:02 +0800

    上传QT模板

---
+++ mywidget.cpp
@@ -0,0 +1,26 @@
+#include "mywidget.h"
+#include "QPushButton"
+
+myWidget::myWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QPushButton *btn1 = new QPushButton(this);
+    QPushButton *btn2 = new QPushButton(this);
+
+    btn1->setText("Close");
+    btn2->setText("按钮2");
+    //connect(btn1,&QPushButton::click,this,&QWidget::close);
+    btn2->move(0,100);
+
+    resize(600,400);
+
+    setWindowTitle("QT程序");
+
+    connect(btn1,&QPushButton::clicked,this,&QWidget::close);
+    //QMetaObject::connectSlotsByName(this);
+}
+
+myWidget::~myWidget()
+{
+}
+
